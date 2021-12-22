eb858368774a9ff0c0208b9283792ad55e1a54f3
khang-nguyen2907 <74083431+khang-nguyen2907@users.noreply.github.com>
2021-12-22 16:40:19 +0700

    Update kl_loss.py

--- utils/kl_loss.py
+++ utils/kl_loss.py
@@ -2,31 +2,30 @@ import tensorflow as tf
 from tensorflow.keras.losses import Loss, KLDivergence
 import numpy as np
 
+def flatten_probas(input, target, labels, ignore_index = 255):
+    input = tf.reshape(input, (-1, C))
+    target = tf.reshape(target, (-1, C))
+    labels = tf.reshape(labels, (-1))
+    mask = (labels==ignore_index)
+    vinput = tf.boolean_mask(input, mask)
+    vtarget = tf.boolean_mask(target, mask)
+    return vinput, vtarget
+
 class KL(Loss):
-    def __init__(self, ignored_index=255):
+    def __init__(self, ignored_index = 255):
         super(KL, self).__init__()
         self.ignored_index = ignored_index
         self.kl = KLDivergence()
+        self.T = 1
 
-    def call(self, y_true, y_pred):
+    def call(self, labels, inputs):
         """
-        loss betweem parsing_pred  and soft_parsing_pred
-
-        y_true -- parsing label or edge label -- b,h,w
-        y_pred -- [parsing_pred, soft_parsing_pred] -- parsing_pred and soft_parsing_pred: b,h,w,c
+        inputs = [input, target]
         """
-        C = y_pred[0].shape[3]
-        inputs = tf.reshape(y_pred[0], (-1, C))  # (b*h*w,C)
-        target = tf.reshape(y_pred[1], (-1, C))  # (b*h*w, C)
-        label = tf.reshape(y_true, (-1))  # (b*h*w)
-
-        valid = tf.not_equal(label, self.ignored_index)
-
-        vy_pred = tf.boolean_mask(inputs, valid)
-        vy_true = tf.boolean_mask(target, valid)
-
-        loss = self.kl(vy_true, vy_pred)
-
+        input = inputs[0]
+        target = inputs[1]
+        log_input_prob = tf.nn.log_softmax(input / self.T, axis=3)
+        target_prob = tf.nn.softmax(target / self.T, axis=3)
+        vinput, vtarget = flatten_probas(log_input_prob, target_prob, labels)
+        loss = self.kl(vtarget,vinput)
         return loss
-
-
